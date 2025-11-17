0993351cfaeb799e837d37231fec33bb4d0892d3
Nefi Zaldana <nefizaldana@gmail.com>
2025-11-16 20:26:32 -0600

    w3_activities

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
  *************************/
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const utilities = require("./utilities/");
 const inventoryRoute = require("./routes/inventoryRoute");
 const baseController = require("./controllers/baseController");
 const env = require("dotenv").config();
@@ -25,11 +26,38 @@ app.set("layout", "./layouts/layout"); // not at views root
  *************************/
 app.use(static);
 
-// Index Route
-app.get("/", baseController.buildHome);
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+/* *********************
+ * File Not Found Route - must be last route in list
+ * Place after all routes
+ * ********************** */
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
