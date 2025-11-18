0262fe19ec208b199a8660e9d7a546342a6f176a
Nefi Zaldana <nefizaldana@gmail.com>
2025-11-17 18:49:16 -0600

    pg_admin

--- database/index.js
+++ database/index.js
@@ -6,32 +6,56 @@ require("dotenv").config();
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool;
-if (process.env.NODE_ENV == "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
 
-  // Added for troubleshooting queries
-  // during development
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params);
+// Connecting to pg database with SSL with Render.com
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
+
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params);
+      if (!isProduction) {
         console.log("executed query", { text });
-        return res;
-      } catch (error) {
-        console.error("error in query", { text });
-        throw error;
       }
-    },
-  };
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-  module.exports = pool;
-}
+      return res;
+    } catch (error) {
+      console.error("error in query", { text, error });
+      throw error;
+    }
+  },
+};
+
+// let pool;
+// if (process.env.NODE_ENV == "development") {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+
+// Added for troubleshooting queries
+// during development
+//   module.exports = {
+//     async query(text, params) {
+//       try {
+//         const res = await pool.query(text, params);
+//         console.log("executed query", { text });
+//         return res;
+//       } catch (error) {
+//         console.error("error in query", { text });
+//         throw error;
+//       }
+//     },
+//   };
+// } else {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   });
+//   module.exports = pool;
+// }
